197035fbc6546521a3ae51d220cf919ed0681a06
vinay <vinay.p9629@gmail.com>
2019-03-09 13:01:53 -0500

    Updated commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/url/:param", (req, res, next) => {
+ res.json(["The","search","query","is",req.params.param]);
+});
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
