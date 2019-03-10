e655d7c56f2f3e11e941d2eb1491081c9d5aad65
vinay <vinay.p9629@gmail.com>
2019-03-10 15:19:53 -0400

    Adding quotes file and code to parse

--- index.js
+++ index.js
@@ -2,9 +2,37 @@ var express = require("express");
 var app = express();
 
 app.get("/url/:param", (req, res, next) => {
- res.json(["The","search","query","is",req.params.param]);
+	var size = 0;
+	var dump;
+	var resultsArray = new Array();
+	const fs = require('fs');
+	fs.readFile('quotes.txt', function(err, data) {
+
+		if(err) {
+			console.log("Error with the file");
+			throw err;
+		}
+		
+
+		// Get an array of lines from the text file
+		var lines = data.toString().split('\n');
+		var keyword = req.params.param.toLowerCase();
+
+
+		console.log(keyword);
+		lines.forEach(function(line){
+			// console.log(line.toLowerCase());
+			if(line.toLowerCase().includes(keyword)){
+				console.log("Found a match");
+				console.log(line.substring(0, 8));
+				resultsArray.push(line.substring(0, 8));
+			}
+		});
+		console.log("result array is \n"+resultsArray.length);
+		res.json(resultsArray);
+	});	
 });
 
 app.listen(process.env.PORT || 3000, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port "+process.env.PORT);
 });
